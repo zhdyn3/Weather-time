e682e933fb4994221b260980eed700436d04e47d
Zhdyn3 <zdanovmma@gmail.com>
2024-07-17 21:32:36 +0500

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,101 +1,148 @@
+using System;
 using BepInEx;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Utilla;
 
-[BepInPlugin("com.Zhdyn3.gorillatag.weathertime", "Weather Time", "1.0.0")]
-public class TimeAndWeatherMod : BaseUnityPlugin
+namespace GorillaTagModTemplateProject
 {
-    private bool uiOpen = false;
-    private bool keyPressed = false;
-
-    private void Update()
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin("com.Zhdyn3.gorillatag.weathertime", "Weather Time", "1.0.0")] // Используйте явно указанный GUID, имя и версию
+    public class Plugin : BaseUnityPlugin
     {
-        if (Keyboard.current.tabKey.isPressed)
+        bool inRoom;
+
+        private bool uiOpen = false;
+        private bool keyPressed = false;
+
+        void Start()
         {
-            if (!keyPressed)
-            {
-                uiOpen = !uiOpen;
-            }
-            keyPressed = true;
+            Utilla.Events.GameInitialized += OnGameInitialized;
         }
-        else
+
+        void OnEnable()
         {
-            keyPressed = false;
+            // Если вы не используете Harmony, удалите следующие строки
+            // HarmonyPatches.ApplyHarmonyPatches();
         }
-    }
 
-    void OnGUI()
-    {
-        if (uiOpen)
+        void OnDisable()
         {
-            float buttonWidth = 180f;
-            float buttonHeight = 30f;
-            float buttonSpacing = 40f;
-            float buttonX = 30f;
-            float buttonY = Screen.height - 210f;
+            // Если вы не используете Harmony, удалите следующие строки
+            // HarmonyPatches.RemoveHarmonyPatches();
+        }
 
-            if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), "Night"))
-            {
-                SetNightTime();
-            }
-            if (GUI.Button(new Rect(buttonX, buttonY + buttonSpacing, buttonWidth, buttonHeight), "Evening"))
-            {
-                SetEveningTime();
-            }
-            if (GUI.Button(new Rect(buttonX, buttonY + 2 * buttonSpacing, buttonWidth, buttonHeight), "Morning"))
-            {
-                SetMorningTime();
-            }
-            if (GUI.Button(new Rect(buttonX, buttonY + 3 * buttonSpacing, buttonWidth, buttonHeight), "Day"))
-            {
-                SetDayTime();
-            }
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            // Инициализация после загрузки игры
+        }
 
-            float weatherButtonX = Screen.width - buttonWidth - 30f;
-            if (GUI.Button(new Rect(weatherButtonX, buttonY, buttonWidth, buttonHeight), "Rain"))
+        void Update()
+        {
+            if (inRoom)
             {
-                SetRain();
-            }
-            if (GUI.Button(new Rect(weatherButtonX, buttonY + buttonSpacing, buttonWidth, buttonHeight), "Clear"))
-            {
-                SetNoRain();
+                if (Keyboard.current.tabKey.isPressed)
+                {
+                    if (!keyPressed)
+                    {
+                        uiOpen = !uiOpen;
+                    }
+                    keyPressed = true;
+                }
+                else
+                {
+                    keyPressed = false;
+                }
             }
         }
-    }
 
-    public static void SetNightTime()
-    {
-        BetterDayNightManager.instance.SetTimeOfDay(0);
-    }
-
-    public static void SetEveningTime()
-    {
-        BetterDayNightManager.instance.SetTimeOfDay(7);
-    }
-
-    public static void SetMorningTime()
-    {
-        BetterDayNightManager.instance.SetTimeOfDay(1);
-    }
-
-    public static void SetDayTime()
-    {
-        BetterDayNightManager.instance.SetTimeOfDay(3);
-    }
-
-    public static void SetRain()
-    {
-        for (int i = 1; i < BetterDayNightManager.instance.weatherCycle.Length; i++)
+        void OnGUI()
         {
-            BetterDayNightManager.instance.weatherCycle[i] = BetterDayNightManager.WeatherType.Raining;
+            if (uiOpen && inRoom)
+            {
+                float buttonWidth = 180f;
+                float buttonHeight = 30f;
+                float buttonSpacing = 40f;
+                float buttonX = 30f;
+                float buttonY = Screen.height - 210f;
+
+                if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), "Night"))
+                {
+                    SetNightTime();
+                }
+                if (GUI.Button(new Rect(buttonX, buttonY + buttonSpacing, buttonWidth, buttonHeight), "Evening"))
+                {
+                    SetEveningTime();
+                }
+                if (GUI.Button(new Rect(buttonX, buttonY + 2 * buttonSpacing, buttonWidth, buttonHeight), "Morning"))
+                {
+                    SetMorningTime();
+                }
+                if (GUI.Button(new Rect(buttonX, buttonY + 3 * buttonSpacing, buttonWidth, buttonHeight), "Day"))
+                {
+                    SetDayTime();
+                }
+
+                float weatherButtonX = Screen.width - buttonWidth - 30f;
+                if (GUI.Button(new Rect(weatherButtonX, buttonY, buttonWidth, buttonHeight), "Rain"))
+                {
+                    SetRain();
+                }
+                if (GUI.Button(new Rect(weatherButtonX, buttonY + buttonSpacing, buttonWidth, buttonHeight), "Clear"))
+                {
+                    SetNoRain();
+                }
+            }
         }
-    }
 
-    public static void SetNoRain()
-    {
-        for (int i = 1; i < BetterDayNightManager.instance.weatherCycle.Length; i++)
+        public static void SetNightTime()
         {
-            BetterDayNightManager.instance.weatherCycle[i] = BetterDayNightManager.WeatherType.None;
+            BetterDayNightManager.instance.SetTimeOfDay(0);
+        }
+
+        public static void SetEveningTime()
+        {
+            BetterDayNightManager.instance.SetTimeOfDay(7);
+        }
+
+        public static void SetMorningTime()
+        {
+            BetterDayNightManager.instance.SetTimeOfDay(1);
+        }
+
+        public static void SetDayTime()
+        {
+            BetterDayNightManager.instance.SetTimeOfDay(3);
+        }
+
+        public static void SetRain()
+        {
+            for (int i = 1; i < BetterDayNightManager.instance.weatherCycle.Length; i++)
+            {
+                BetterDayNightManager.instance.weatherCycle[i] = BetterDayNightManager.WeatherType.Raining;
+            }
+        }
+
+        public static void SetNoRain()
+        {
+            for (int i = 1; i < BetterDayNightManager.instance.weatherCycle.Length; i++)
+            {
+                BetterDayNightManager.instance.weatherCycle[i] = BetterDayNightManager.WeatherType.None;
+            }
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
         }
     }
 }
+
